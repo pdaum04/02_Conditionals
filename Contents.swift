e61b05ad629e071897a09eddf1d95543b013c84d
Apple <apple@Apples-MacBook-Pro-89.local>
2019-07-22 12:19:42 -0700

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+ //Intention
+
+
+// <= less than or equal to
+// >= greater than or equal to
+7<=6+1
+
+// test == operator
+// ! means negation
+12 == 10+2
+12 != 10+2
+
+
+
+
+// check what && does
+12 == 10+2 // true
+12 == 6+6 //true
+12 == 10+2 && 12 == 6+6
+12 == 10+2 && 12 == 10+10
+// && checks if both sides are true, if they are true, it returns true
+
+
+
+
+
+// check what these || --> OR
+12 == 10+2 //true
+12 == 10+10 //false
+12 == 10+2 || 12 == 10+10 //left is true but the right is false
+1+2 == 1 || 2+1 == 2
+
+5<3
+12>7
+6 != 8
+"karlie"=="karlie"
+"karle" == "karliekloss"
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+
+
+// declaring an if statement
+var dogAge = 12
+if dogAge < 2 { //checking if dogAge is less than 2 which is true
+    print("You are a puppy 🐶") //if the condition is true, then do this
+} else if dogAge<6{
+    print("You are an average dog!")
+} else { //last statement
+    print("You are elderly")
+}
+
+
+
+
+var favoriteFood = "gluten free pasta"
+if favoriteFood == "brocoli" {
+    print("hi there")
+} else if favoriteFood == "cheese" {
+    print("🐭")
+} else if favoriteFood == "gluten free pasta" {
+    print("join the party")
+}
+
